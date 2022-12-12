6b5b882a935136b53108a0dd33cdf7bd8f060081
Nuno Grazina <101281946+nhorta-g@users.noreply.github.com>
2022-12-12 19:35:14 +0000

    Add files via upload

--- src/small_sort.c
+++ src/small_sort.c
@@ -12,45 +12,69 @@
 
 #include "../includes/push_swap.h"
 
-void	sort2(t_list **stk)
+void	sort2(t_list **a)
 {
-	if ((*stk)->value > (*stk)->next->value)
-		sa(stk);
+	if ((*a)->value > (*a)->next->value)
+		sa(a);
 }
 
-void	sort3(t_list **stk)
+void	sort3(t_list **a)
 {
-	if (((*stk)->value < (*stk)->next->value) && ((*stk)->next->value > \
-	(*stk)->next->next->value) && ((*stk)->value > (*stk)->next->next->value))
-		ra(stk);
-	if (((*stk)->value < (*stk)->next->value) && ((*stk)->next->value > \
-	(*stk)->next->next->value) && ((*stk)->value < (*stk)->next->next->value))
+	if (((*a)->value < (*a)->next->value) && ((*a)->next->value > \
+	(*a)->next->next->value) && ((*a)->value > (*a)->next->next->value))
+		ra(a);
+	if (((*a)->value < (*a)->next->value) && ((*a)->next->value > \
+	(*a)->next->next->value) && ((*a)->value < (*a)->next->next->value))
 	{
-		ra(stk);
-		sa(stk);
+		sa(a);
+		ra(a);
 	}
-	if (((*stk)->value > (*stk)->next->value) && ((*stk)->next->value < \
-	(*stk)->next->next->value) && ((*stk)->value < (*stk)->next->next->value))
-		sa(stk);
-	if (((*stk)->value > (*stk)->next->value) && ((*stk)->next->value < \
-	(*stk)->next->next->value) && ((*stk)->value > (*stk)->next->next->value))
-		ra(stk);
-	if (((*stk)->value > (*stk)->next->value) && ((*stk)->next->value > \
-	(*stk)->next->next->value) && ((*stk)->value > (*stk)->next->next->value))
+	if (((*a)->value > (*a)->next->value) && ((*a)->next->value < \
+	(*a)->next->next->value) && ((*a)->value < (*a)->next->next->value))
+		sa(a);
+	if (((*a)->value > (*a)->next->value) && ((*a)->next->value < \
+	(*a)->next->next->value) && ((*a)->value > (*a)->next->next->value))
+		ra(a);
+	if (((*a)->value > (*a)->next->value) && ((*a)->next->value > \
+	(*a)->next->next->value) && ((*a)->value > (*a)->next->next->value))
 	{
-		sa(stk);
-		rra(stk);
+		sa(a);
+		rra(a);
 	}
 	return ;
 }
-/*
-void	sort4(t_list **stk)
+
+void	sort5(t_list **a, t_list **b)
 {
-	exit;
+
 }
 
-void	sort5(t_list **stk)
+void	find_min(t_lst **stack, int *min)
 {
-	exit;
+	t_lst	*temp;
+
+	temp = (*stack);
+	*min = (*stack)->number;
+	while (temp->next)
+	{
+		temp = temp->next;
+		if (temp->number <= *min)
+			*min = temp->number;
+	}
+	return ;
 }
-*/
+
+void	find_max(t_lst **stack, int *max)
+{
+	t_lst	*temp;
+
+	temp = (*stack);
+	*max = (*stack)->number;
+	while (temp->next)
+	{
+		temp = temp->next;
+		if (temp->number >= *max)
+			*max = temp->number;
+	}
+	return ;
+}
